d0448d0f3a031e987f5afd5bfd860752f37e4674
Ash <ashley.healy@codeclan.com>
2023-08-15 12:24:07 +0100

    updates

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LoginApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Login App!");
+
+            Console.Write("Username: ");
+            string username = Console.ReadLine();
+
+            Console.Write("Password: ");
+            string password = Console.ReadLine();
+
+            if (username == "user" && password == "password")
+            {
+                Console.WriteLine("Login successful!");
+            }
+            else
+            {
+                Console.WriteLine("Login failed. Please try again.");
+            }
+        }
+    }
+}
